19fb174574cc66a8e84785d45bc87b44c4966299
Unknown <vrushali@absentiavr.com>
2018-02-15 20:28:59 +0530

    base

---
+++ index.js
@@ -0,0 +1,20 @@
+var port = process.env.PORT || 8000;
+var http = require('http');
+var express = require('express');
+var app = express();
+var server = http.createServer(app).listen(port);
+var bodyParser = require('body-parser')
+var connectionCount = 0;
+app.use(bodyParser.json());
+// app.use(express.static(__dirname + '/www'));
+app.post('/json', function(req, res) {
+    console.log(req.body);
+    res.sendStatus(200)
+});
+app.get('/json', function(req, res) {
+    console.log(req.body);
+    res.sendStatus(200)
+});
+server.listen(port, function() {
+    console.log("Listening on port " + port + "...");
+});
